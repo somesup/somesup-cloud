fb96572c97260b15c65d19ad0c902f6fe448e83f
Park Junwoo <boksam1017@gmail.com>
2025-05-10 15:35:37 +0900

    add sql_admin api

--- apis.tf
+++ apis.tf
@@ -2,3 +2,8 @@ resource "google_project_service" "secret_manager" {
   project = var.project
   service = "secretmanager.googleapis.com"
 }
+
+resource "google_project_service" "sql_admin" {
+  project = var.project
+  service = "sqladmin.googleapis.com"
+}
