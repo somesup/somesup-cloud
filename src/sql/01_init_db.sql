6f32ac7afb8647d08538aeddda53143700b42125
Junwoo Park <64635737+Boksam@users.noreply.github.com>
2025-05-27 15:44:00 +0900

    [Terraform] Article - Processed Article 테이블 간 관계 변경 (#15)

--- src/sql/01_init_db.sql
+++ src/sql/01_init_db.sql
@@ -7,24 +7,6 @@ CREATE TABLE somesup.article_provider (
   PRIMARY KEY (id)
 );
 
-CREATE TABLE somesup.article (
-  id            INT           NOT NULL AUTO_INCREMENT,
-  provider_id   INT           NOT NULL,
-  title         VARCHAR(255)  NOT NULL,
-  content       TEXT          NOT NULL,
-  language      VARCHAR(30)   NOT NULL,
-  region        VARCHAR(30)   NULL,
-  section       VARCHAR(30)   NULL,
-  thumbnail_url VARCHAR(255)  NOT NULL,
-  news_url      VARCHAR(255)  NOT NULL,
-  is_processed  BOOLEAN       NOT NULL DEFAULT FALSE,
-  created_at    DATETIME      NOT NULL DEFAULT CURRENT_TIMESTAMP,
-
-  PRIMARY KEY (id),
-
-  FOREIGN KEY (provider_id) REFERENCES article_provider(id)
-);
-
 CREATE TABLE somesup.processed_article (
   id                INT           NOT NULL AUTO_INCREMENT,
   title             VARCHAR(255)  NOT NULL,
@@ -38,11 +20,22 @@ CREATE TABLE somesup.processed_article (
   PRIMARY KEY (id)
 );
 
-CREATE TABLE somesup.article_processed_mapping (
-  article_id    INT           NOT NULL,
-  processed_id  INT           NOT NULL,
-  mapped_at     DATETIME      NOT NULL DEFAULT CURRENT_TIMESTAMP,
+CREATE TABLE somesup.article (
+  id            INT           NOT NULL AUTO_INCREMENT,
+  provider_id   INT           NOT NULL,
+  processed_id  INT           NULL,
+  title         VARCHAR(255)  NOT NULL,
+  content       TEXT          NOT NULL,
+  language      VARCHAR(30)   NOT NULL,
+  region        VARCHAR(30)   NULL,
+  section       VARCHAR(30)   NULL,
+  thumbnail_url VARCHAR(255)  NOT NULL,
+  news_url      VARCHAR(255)  NOT NULL,
+  is_processed  BOOLEAN       NOT NULL DEFAULT FALSE,
+  created_at    DATETIME      NOT NULL DEFAULT CURRENT_TIMESTAMP,
 
-  FOREIGN KEY (article_id)    REFERENCES article(id),
-  FOREIGN KEY (processed_id)  REFERENCES processed_article(id)
+  PRIMARY KEY (id),
+
+  FOREIGN KEY (provider_id) REFERENCES article_provider(id),
+  FOREIGN KEY (processed_id) REFERENCES processed_article(id)
 );
