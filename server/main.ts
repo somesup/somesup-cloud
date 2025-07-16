1364fa6fb633c08dd1b9f81b6b456db18576cfa9
Junwoo Park <64635737+Boksam@users.noreply.github.com>
2025-07-17 01:26:13 +0900

    [Server] Auth & User 라우트 설정 및 연결 (#47)
    * connect auth user route
    
    * fix schema fault & set up env first
    
    * change changing user nickname route path

--- server/main.ts
+++ server/main.ts
@@ -1,14 +1,22 @@
-import Express from 'express'
 import dotenv from 'dotenv'
+dotenv.config()
+
+import Express from 'express'
+
+import { redisClient } from './src/config/redis'
 
 import articleRouter from './src/routes/article'
+import authRouter from './src/routes/auth'
+import userRouter from './src/routes/user'
 
-dotenv.config()
+redisClient.connect()
 
 const app = Express()
 app.use(Express.json())
 
 app.use('/api/articles', articleRouter)
+app.use('/api/auth', authRouter)
+app.use('/api/users', userRouter)
 
 const PORT = process.env.PORT || 3000
 
