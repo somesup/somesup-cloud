fad6edaa39ab74e3fa4ff9d9b435810523198d31
Park Junwoo <boksam1017@gmail.com>
2025-07-26 16:52:59 +0900

    improve patch user info

--- server/src/controllers/userController.ts
+++ server/src/controllers/userController.ts
@@ -2,7 +2,7 @@ import { z } from 'zod'
 import { Response } from 'express'
 import { errors, success } from '../utils/response'
 import { AuthenticatedRequest } from '../middlewares/authenticateJWT'
-import { userService } from '../services/userService'
+import { UserNotFoundError, userService } from '../services/userService'
 
 /**
  * 사용자의 닉네임을 업데이트하는 컨트롤러입니다.
@@ -64,26 +64,33 @@ export type UpdateUserRequest = {
  * @returns {Promise<Response>} 업데이트된 사용자 정보를 포함한 응답 객체
  */
 export const updateUser = async (req: AuthenticatedRequest, res: Response): Promise<Response> => {
-  const userId = req.userId
-  if (!userId) {
-    return errors.unauthorized(res, 'User ID is required')
-  }
+  try {
+    const userId = req.userId
+    if (!userId) {
+      return errors.unauthorized(res, 'User ID is required')
+    }
 
-  // NOTE: 악의적으로 인증 여부 등을 수정하는 것을 막기 위해 체크
-  const updateUserSchema = z
-    .object({
-      nickname: z.string().optional(),
+    // NOTE: 악의적으로 인증 여부 등을 수정하는 것을 막기 위해 체크
+    const updateUserSchema = z
+      .object({
+        nickname: z.string().optional(),
+      })
+      .strict()
+
+    const parsed = updateUserSchema.safeParse(req.body)
+    if (!parsed.success) {
+      return errors.badRequest(res, 'Invalid request body, possibly due to unsupported fields')
+    }
+
+    const user = await userService.updateUserInfo(userId, parsed.data)
+
+    return success(res, user, {
+      message: 'User information updated successfully',
     })
-    .strict()
-
-  const parsed = updateUserSchema.safeParse(req.body)
-  if (!parsed.success) {
-    return errors.badRequest(res, 'Invalid request body, possibly wrong field names')
+  } catch (error) {
+    if (error instanceof UserNotFoundError) {
+      return errors.notFound(res, 'User not found')
+    }
+    return errors.internal(res)
   }
-
-  const user = await userService.updateUserInfo(userId, parsed.data)
-
-  return success(res, user, {
-    message: 'User information updated successfully',
-  })
 }
