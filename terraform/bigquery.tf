79cba6fcbbefa76f85a3228aa3002725f0428880
Junwoo Park <64635737+Boksam@users.noreply.github.com>
2025-08-07 23:58:17 +0900

    [Terraform] ProcessedArticle의 Embedding Vector를 BigQuery에 저장 (#72)
    * store embedding into bigquery
    
    * resolve comments

---
+++ terraform/bigquery.tf
@@ -0,0 +1,33 @@
+resource "google_bigquery_dataset" "recommendation" {
+  project     = var.project
+  dataset_id  = "recommendation"
+  location    = var.region
+  description = "Dataset for article recommendations"
+}
+
+resource "google_bigquery_table" "p_article_embeddings" {
+  project             = var.project
+  dataset_id          = google_bigquery_dataset.recommendation.dataset_id
+  table_id            = "p_article_embeddings"
+  schema              = file("${path.module}/src/schema/p_article_embeddings.json")
+  deletion_protection = false
+
+  time_partitioning {
+    type  = "DAY"
+    field = "created_at"
+  }
+
+}
+
+resource "google_bigquery_table" "user_p_article_scores" {
+  project             = var.project
+  dataset_id          = google_bigquery_dataset.recommendation.dataset_id
+  table_id            = "user_embeddings"
+  schema              = file("${path.module}/src/schema/user_embeddings.json")
+  deletion_protection = false
+
+  time_partitioning {
+    type  = "DAY"
+    field = "updated_at"
+  }
+}
