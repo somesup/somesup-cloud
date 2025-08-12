43436852b81e6f5db46d8771a8145f5c3323e96a
Junwoo Park <64635737+Boksam@users.noreply.github.com>
2025-08-12 23:24:05 +0900

    [Terraform] 뉴스사 이미지 GCS 버킷에 저장 및 Provider, Section 한글 이름 추가 (#86)
    * store provider images
    
    * add friend-name on provider
    
    * add default providers in seed
    
    * add friendly name & refactoring seed.ts

--- server/prisma/seed.ts
+++ server/prisma/seed.ts
@@ -2,6 +2,11 @@ import { PrismaClient, SectionType } from '@prisma/client'
 
 const prisma = new PrismaClient()
 
+async function main() {
+  await createSections()
+  await createProviders()
+}
+
 /**
  * 기본 뉴스 섹션을 데이터베이스에 추가하는 스크립트입니다.
  * 이 스크립트는 뉴스 섹션을 데이터베이스에 삽입하거나 이미 존재하는 경우 업데이트합니다.
@@ -13,21 +18,137 @@ const prisma = new PrismaClient()
  *   기술 (tech)
  *   세계 (world)
  */
-async function main() {
+const createSections = async () => {
   const sections = [
-    SectionType.politics,
-    SectionType.economy,
-    SectionType.society,
-    SectionType.culture,
-    SectionType.tech,
-    SectionType.world,
+    { name: SectionType.politics, friendlyName: '정치' },
+    { name: SectionType.economy, friendlyName: '경제' },
+    { name: SectionType.society, friendlyName: '사회' },
+    { name: SectionType.culture, friendlyName: '문화' },
+    { name: SectionType.tech, friendlyName: '기술' },
+    { name: SectionType.world, friendlyName: '세계' },
   ]
 
-  for (const name of sections) {
+  for (const section of sections) {
     await prisma.articleSection.upsert({
-      where: { name },
-      update: {}, // 이미 있으면 변경하지 않음
-      create: { name },
+      where: { name: section.name },
+      update: {},
+      create: {
+        name: section.name,
+        friendly_name: section.friendlyName,
+      },
+    })
+  }
+}
+
+/**
+ * 기본 뉴스 제공자를 데이터베이스에 추가하는 스크립트입니다.
+ * 이 스크립트는 뉴스 제공자를 데이터베이스에 삽입하거나 이미 존재하는 경우 업데이트합니다.
+ * 각 제공자는 고유한 ID, 이름, 친숙한 이름 및 로고 URL을 가지고 있습니다.
+ */
+const createProviders = async () => {
+  const providers = [
+    {
+      id: 1,
+      name: '오마이뉴스',
+      friendlyName: '오마이뉴스',
+      logoUrl: 'https://storage.googleapis.com/somesup-462506-provider-logo/1.png',
+    },
+    {
+      id: 2,
+      name: 'The Guardian',
+      friendlyName: 'The Guardian',
+      logoUrl: 'https://storage.googleapis.com/somesup-462506-provider-logo/2.png',
+    },
+    {
+      id: 3,
+      name: '중앙일보',
+      friendlyName: '중앙일보',
+      logoUrl: 'https://storage.googleapis.com/somesup-462506-provider-logo/3.png',
+    },
+    {
+      id: 4,
+      name: 'Chosun.com',
+      friendlyName: '조선일보',
+      logoUrl: 'https://storage.googleapis.com/somesup-462506-provider-logo/4.png',
+    },
+    {
+      id: 5,
+      name: 'YTN',
+      friendlyName: 'YTN',
+      logoUrl: 'https://storage.googleapis.com/somesup-462506-provider-logo/5.png',
+    },
+    {
+      id: 6,
+      name: 'www.donga.com',
+      friendlyName: '동아일보',
+      logoUrl: 'https://storage.googleapis.com/somesup-462506-provider-logo/6.png',
+    },
+    {
+      id: 7,
+      name: '국민일보',
+      friendlyName: '국민일보',
+      logoUrl: 'https://storage.googleapis.com/somesup-462506-provider-logo/7.png',
+    },
+    {
+      id: 8,
+      name: '경향신문',
+      friendlyName: '경향신문',
+      logoUrl: 'https://storage.googleapis.com/somesup-462506-provider-logo/8.png',
+    },
+    {
+      id: 9,
+      name: 'The Wall Street Journal',
+      friendlyName: 'The Wall Street Journal',
+      logoUrl: 'https://storage.googleapis.com/somesup-462506-provider-logo/9.png',
+    },
+    {
+      id: 10,
+      name: 'Daily Mail Online',
+      friendlyName: 'Daily Mail',
+      logoUrl: 'https://storage.googleapis.com/somesup-462506-provider-logo/10.png',
+    },
+    {
+      id: 11,
+      name: 'BBC',
+      friendlyName: 'BBC',
+      logoUrl: 'https://storage.googleapis.com/somesup-462506-provider-logo/11.png',
+    },
+    {
+      id: 12,
+      name: 'Reuters',
+      friendlyName: 'Reuters',
+      logoUrl: 'https://storage.googleapis.com/somesup-462506-provider-logo/12.png',
+    },
+    {
+      id: 13,
+      name: 'AP NEWS',
+      friendlyName: 'AP NEWS',
+      logoUrl: 'https://storage.googleapis.com/somesup-462506-provider-logo/13.png',
+    },
+    {
+      id: 14,
+      name: 'Breitbart',
+      friendlyName: 'Breitbart',
+      logoUrl: 'https://storage.googleapis.com/somesup-462506-provider-logo/14.png',
+    },
+    {
+      id: 15,
+      name: 'Fox News',
+      friendlyName: 'Fox News',
+      logoUrl: 'https://storage.googleapis.com/somesup-462506-provider-logo/15.png',
+    },
+  ]
+
+  for (const provider of providers) {
+    await prisma.articleProvider.upsert({
+      where: { id: provider.id },
+      update: {},
+      create: {
+        id: provider.id,
+        name: provider.name,
+        friendly_name: provider.friendlyName,
+        logo_url: provider.logoUrl,
+      },
     })
   }
 }
--- terraform/buckets.tf
+++ terraform/buckets.tf
@@ -4,3 +4,29 @@ resource "google_storage_bucket" "cloud_function" {
   name                        = "${var.project}-cloud-function-source"
   uniform_bucket_level_access = true
 }
+
+resource "google_storage_bucket" "provider_logo" {
+  project                     = var.project
+  location                    = var.region
+  name                        = "${var.project}-provider-logo"
+  uniform_bucket_level_access = true
+}
+
+resource "google_storage_bucket_iam_member" "public_read" {
+  bucket = google_storage_bucket.provider_logo.name
+  role   = "roles/storage.objectViewer"
+  member = "allUsers" # 공개 접근 허용
+}
+
+resource "null_resource" "upload_provider_logos" {
+  provisioner "local-exec" {
+    command = <<-EOT
+      gsutil -m rm -r ${google_storage_bucket.provider_logo.url}/*
+      gsutil -m cp -r assets/provider_logos/* ${google_storage_bucket.provider_logo.url}
+    EOT
+  }
+
+  triggers = {
+    dir_hash = sha1(join("", [for f in fileset("assets/provider_logos", "**") : filesha1("${path.module}/assets/provider_logos/${f}")]))
+  }
+}
